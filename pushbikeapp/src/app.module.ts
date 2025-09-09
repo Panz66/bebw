a30c0536f2d0a1934824ac6ef58a7360561c6bcd
Ipan Wardana <ipanwardana123@gmail.com>
2025-09-10 03:40:15 +0700

    third commit

--- pushbikeapp/src/app.module.ts
+++ pushbikeapp/src/app.module.ts
@@ -3,7 +3,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
 import { EmailModule } from './email/email.module';
 import { LombaModule } from './lomba/lomba.module';
 import { PesertaModule } from './peserta/peserta.module';
@@ -14,17 +13,17 @@ import { StatistikModule } from './statistik/statistik.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost', // default ke localhost (XAMPP)
-      port: parseInt(process.env.DB_PORT || '3306', 10),
-      username: process.env.DB_USERNAME || 'root', // default root
-      password: process.env.DB_PASSWORD || '',      // default kosong di XAMPP
-      database: process.env.DB_DATABASE || 'pushbikeweb', // default db name
-      entities: [User],
-      synchronize: false, // ⚠️ aktifkan hanya untuk development
-      autoLoadEntities: true, // supaya semua entity diimport otomatis
-      migrationsRun:true,
-    }),
+  type: 'mysql',
+  host: process.env.MYSQLHOST, 
+  port: parseInt(process.env.MYSQLPORT || '3306', 10),
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  autoLoadEntities: true,
+  synchronize: false,
+  migrationsRun: true,
+}),
+
     UsersModule,
     EmailModule,
     LombaModule,
